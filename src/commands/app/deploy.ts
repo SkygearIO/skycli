c09c141d417ec0f2450ee3158fbc7ca7a0817954
Steven Chan <stevenchan@oursky.com>
2019-04-08 20:50:40 +0800

    Create cloud code src archive

--- src/commands/app/deploy.ts
+++ src/commands/app/deploy.ts
@@ -1,15 +1,34 @@
 import chalk from 'chalk';
+import os from 'os';
+import path from 'path';
+import tar from 'tar';
+import { isArray } from 'util';
 
 import { Arguments, createCommand } from '../../util';
 import requireUser from '../middleware/requireUser';
 
+function archivePath() {
+  return path.join(os.tmpdir(), 'skygear-src.tgz');
+}
+
+function archiveSrc(srcPath: string | string[]) {
+  const opt = {
+    file: archivePath(),
+    gzip: true
+  };
+  return tar.c(opt, isArray(srcPath) ? srcPath : [srcPath]);
+}
+
 function run(argv: Arguments) {
   console.log(chalk`Deploy cloud code to app: {green ${argv.context.app}}`);
-  const cloudCode = argv.appConfig.cloudCode || {};
-  Object.keys(cloudCode)
-    .map((cloudCodeName) => {
-      console.log(chalk`Deploying cloud code: {green ${cloudCodeName}}`);
+  const cloudCodeMap = argv.appConfig.cloudCode || {};
+  Object.keys(cloudCodeMap).map((cloudCodeName) => {
+    console.log(chalk`Deploying cloud code: {green ${cloudCodeName}}`);
+    const cloudCode = cloudCodeMap[cloudCodeName];
+    archiveSrc(cloudCode.src).then(() => {
+      console.log('Archive created');
     });
+  });
   return Promise.resolve();
 }
 
