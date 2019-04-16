46982c3dac0a0d9a634ba09785f0137e066bdd34
Steven Chan <stevenchan@oursky.com>
2019-04-16 19:37:06 +0800

    Add dependency to cloud code demo

--- example/demo/js/hello/index.js
+++ example/demo/js/hello/index.js
@@ -1,6 +1,8 @@
+const _ = require('lodash');
+
 module.exports = async function (context) {
   return {
     status: 200,
-    body: "Hello, world!\n"
+    body: `Hello world: ${_.random(false)}\n`
   };
 }
