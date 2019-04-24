174a7be8f882ba88e3190bcb75634fa0d42bc542
Steven Chan <stevenchan@oursky.com>
2019-04-24 15:35:01 +0800

    Wait for cloud code status change when deploy

--- src/api/cloudcode.ts
+++ src/api/cloudcode.ts
@@ -1,4 +1,5 @@
 import { CLIContext } from '../types';
+import { CloudCode, cloudCodeFromJSON } from '../types/cloudCode';
 import {
   CloudCodeConfig,
   createCloudCodeRequestPayloadFromConfig
@@ -10,9 +11,22 @@ export async function createCloudCode(
   name: string,
   cloudCode: CloudCodeConfig,
   artifactID: string
-) {
+): Promise<string> {
   return callAPI(context, '/_controller/cloud_code', 'POST', {
     app_name: context.app,
     ...createCloudCodeRequestPayloadFromConfig(name, cloudCode, artifactID)
+  }).then((payload) => {
+    return payload.result.cloud_code.id;
   });
+}
+
+export async function getCloudCode(
+  context: CLIContext,
+  cloudCodeID: string
+): Promise<CloudCode> {
+  return callAPI(context, `/_controller/cloud_code/${cloudCodeID}`, 'GET').then(
+    (payload) => {
+      return cloudCodeFromJSON(payload.result.cloud_code);
+    }
+  );
 }
