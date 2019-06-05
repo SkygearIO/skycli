b4344b9f9ccafa726eaa6c25d72aaed593e7be0c
Louis Chan <louischan@oursky.com>
2019-06-05 15:30:56 +0800

    Work around yargs Middleware type definition
    At runtime, yargs supports async middleware but
    the type definition does not declare this ability.
    So we cast it to MiddlewareFunction.

--- src/commands/middleware/requireApp.ts
+++ src/commands/middleware/requireApp.ts
@@ -1,12 +1,15 @@
 import chalk from 'chalk';
+import { MiddlewareFunction } from 'yargs';
 import { Arguments } from '../../util';
 
-export default function requireApp(argv: Arguments) {
+function requireApp(argv: Arguments): Promise<void> {
   if (argv.context.app) {
-    return;
+    return Promise.resolve();
   }
 
   return Promise.reject(chalk`{red ERROR:} Requires skygear.yaml, please setup app directory.
 To setup, please run:
     skycli app scaffold`);
 }
+
+export default (requireApp as any) as MiddlewareFunction;
--- src/commands/middleware/requireClusterConfig.ts
+++ src/commands/middleware/requireClusterConfig.ts
@@ -1,12 +1,15 @@
 import chalk from 'chalk';
+import { MiddlewareFunction } from 'yargs';
 import { Arguments } from '../../util';
 
-export default function requireClusterConfig(argv: Arguments) {
+function requireClusterConfig(argv: Arguments): Promise<void> {
   if (argv.context.cluster && argv.context.cluster.endpoint) {
-    return;
+    return Promise.resolve();
   }
 
   return Promise.reject(chalk`{red ERROR:} Missing cluster server endpoint.
 To setup, please run:
     skycli config set-cluster-server`);
 }
+
+export default (requireClusterConfig as any) as MiddlewareFunction;
