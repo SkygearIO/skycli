bf3fb098f34f49eb821c3f492a88e052bc16077d
elly0t <elliotwong@oursky.com>
2019-09-10 16:59:33 +0800

    update demo code with skygear next sdk 2.0.0-alpha

--- example/demo/express/api/write_blog.js
+++ example/demo/express/api/write_blog.js
@@ -1,27 +1,28 @@
 const MongoClient = require('mongodb').MongoClient;
 
-module.exports = async function (req, res) {
+module.exports = async function(req, res) {
   try {
-    const userID = req.body.args.userID;
-    const title = req.body.args.title;
-    const content = req.body.args.content;
-    
     const client = await connectDB();
+
+    const userID = req.body.userID;
+    const title = req.body.title;
+    const content = req.body.content;
+
     await saveBlog(client, userID, title, content);
     client.close();
-    
+
     res.send('OK');
   } catch (err) {
     throw err;
   }
-}
+};
 
 function connectDB() {
   return new Promise((resolve, reject) => {
     const uri = process.env.MONGO_DB_URL;
     const client = new MongoClient(uri, { useNewUrlParser: true });
 
-    client.connect(err => {
+    client.connect((err) => {
       if (err) {
         reject(err);
         return;
@@ -38,7 +39,7 @@ function saveBlog(client, userID, title, content) {
     collection.insertOne(data, (err, result) => {
       if (err) {
         reject(err);
-        return
+        return;
       }
 
       resolve(result);
