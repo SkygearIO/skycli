8da99b76eaf08c642deb096a8ce77558cda5003f
kiootic <kiootic@oursky.com>
2019-10-18 13:37:16 +0800

    Use XDG convention on macOS

--- src/path.ts
+++ src/path.ts
@@ -1,16 +1,34 @@
-import envPath from 'env-paths';
-import { join } from 'path';
+import { join, basename } from 'path';
+import { homedir, tmpdir } from 'os';
 
-const skycliPaths = envPath('skycli', { suffix: '' });
+const home = homedir();
+const tmp = tmpdir();
+const paths = {
+  config: join(process.env.XDG_CONFIG_HOME || join(home, '.config'), 'skycli'),
+  data: join(
+    process.env.XDG_DATA_HOME || join(home, '.local', 'share'),
+    'skycli'
+  ),
+  temp: join(tmp, basename(home), 'skycli')
+};
+
+if (process.platform === 'win32') {
+  const appData = process.env.APPDATA || join(home, 'AppData', 'Roaming');
+  const localAppData =
+    process.env.LOCALAPPDATA || join(home, 'AppData', 'Local');
+  paths.config = join(appData, 'skycli', 'Config');
+  paths.data = join(localAppData, 'skycli', 'Data');
+  paths.temp = join(tmp, 'skycli');
+}
 
 export function configPath(...relPaths: string[]): string {
-  return join(skycliPaths.config, ...relPaths);
+  return join(paths.config, ...relPaths);
 }
 
 export function dataPath(...relPaths: string[]): string {
-  return join(skycliPaths.data, ...relPaths);
+  return join(paths.data, ...relPaths);
 }
 
 export function tempPath(...relPaths: string[]): string {
-  return join(skycliPaths.temp, ...relPaths);
+  return join(paths.temp, ...relPaths);
 }
