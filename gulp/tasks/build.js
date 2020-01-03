d78b1f9b1544178b3f56a5be3e6a44dd1fd4bdb1
Louis Chan <louischan@oursky.com>
2020-01-03 15:26:58 +0800

    Remove gulp-based build system

--- gulp/tasks/build.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var babel = require('gulp-babel');
-
-gulp.task('build', () => {
-  return gulp.src(['src/**/*.js', 'src/**/*.ts'])
-    .pipe(babel())
-    .pipe(gulp.dest('dist'));
-});
--- gulp/tasks/watch.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-require('./build');
-
-gulp.task('watch', gulp.series('build', () => {
-  return gulp.watch([
-    'src/**/*.js',
-    'src/**/*.ts'
-  ], gulp.series('build'));
-}));
