5b3d9e24a1b6613e534b564b345df9e8b4d2fdb8
Louis Chan <louischan@oursky.com>
2020-01-14 18:16:24 +0800

    Upgrade babel to 7.8 to support optional chaining

--- babel.config.js
+++ babel.config.js
@@ -1,70 +1,24 @@
 var plugins = [
-  // TypeScript section begin
   "@babel/plugin-transform-typescript",
   "@babel/plugin-proposal-class-properties",
-  // TypeScript section end
-  // Inlined preset-env section begin
-  // The order is obtained by specifying debug: true
-  "@babel/plugin-transform-template-literals",
-  "@babel/plugin-transform-literals",
-  "@babel/plugin-transform-function-name",
-  "@babel/plugin-transform-arrow-functions",
-  "@babel/plugin-transform-block-scoped-functions",
-  "@babel/plugin-transform-classes",
-  "@babel/plugin-transform-object-super",
-  "@babel/plugin-transform-shorthand-properties",
-  "@babel/plugin-transform-duplicate-keys",
-  "@babel/plugin-transform-computed-properties",
+];
+
+var presets = [
   [
-    "@babel/plugin-transform-for-of",
+    "@babel/preset-env",
     {
-      assumeArray: true,
-    },
-  ],
-  "@babel/plugin-transform-sticky-regex",
-  "@babel/plugin-transform-dotall-regex",
-  "@babel/plugin-transform-unicode-regex",
-  [
-    "@babel/plugin-transform-spread",
-    {
+      targets: {
+        node: "10",
+      },
       loose: true,
-    },
-  ],
-  "@babel/plugin-transform-parameters",
-  "@babel/plugin-transform-destructuring",
-  "@babel/plugin-transform-block-scoping",
-  "@babel/plugin-transform-typeof-symbol",
-  "@babel/plugin-transform-new-target",
-  "@babel/plugin-transform-regenerator",
-  "@babel/plugin-transform-exponentiation-operator",
-  "@babel/plugin-transform-async-to-generator",
-  "@babel/plugin-proposal-async-generator-functions",
-  "@babel/plugin-proposal-object-rest-spread",
-  "@babel/plugin-proposal-unicode-property-regex",
-  "@babel/plugin-proposal-json-strings",
-  "@babel/plugin-proposal-optional-catch-binding",
-  "@babel/plugin-transform-named-capturing-groups-regex",
-  "@babel/plugin-transform-member-expression-literals",
-  "@babel/plugin-transform-property-literals",
-  "@babel/plugin-transform-reserved-words",
-  // Inlined preset-env section end
-  // Use tranform-runtime to avoid polluting the global namespace.
-  [
-    "@babel/plugin-transform-runtime",
-    {
-      // During test, we do not use module
-      // because jest by default does not transform node_modules.
-      // useESModules will cause module source code to be loaded,
-      // which is not parsable by jest.
-      useESModules: process.env.NODE_ENV === "test" ? false : true,
+      modules: process.env.NODE_ENV === "test" ? "commonjs" : false,
+      useBuiltIns: "usage",
+      corejs: 3,
     },
   ],
 ];
 
-if (process.env.NODE_ENV === "test") {
-  plugins.push("@babel/plugin-transform-modules-commonjs");
-}
-
 module.exports = {
   plugins,
+  presets,
 };
