a31796156151c937fb802eb43ca0a89b060356f0
kiootic <kiootic@oursky.com>
2020-02-21 13:12:43 +0800

    Show raw YAML in config view command

--- src/commands/config/view.ts
+++ src/commands/config/view.ts
@@ -1,34 +1,9 @@
-import Table, { VerticalTable } from "cli-table3";
-import { ClusterConfig } from "../../types";
+import * as yaml from "js-yaml";
 import { Arguments, createCommand } from "../../util";
 
-function createVerticalTableRow(
-  key: string,
-  value: string
-): Table.VerticalTableRow {
-  return { [key]: value };
-}
-
 function run(argv: Arguments) {
-  const cluster = (argv.context.cluster || {}) as ClusterConfig;
-  const userContext = argv.context.user;
-  const table = new Table({
-    head: ["Property", "Value"],
-  }) as VerticalTable;
-
-  const userEmail =
-    userContext &&
-    "loginID" in userContext.identity &&
-    userContext.identity.loginID;
-
-  table.push(
-    createVerticalTableRow("Cluster Type", cluster.env || ""),
-    createVerticalTableRow("Cluster Endpoint", cluster.endpoint),
-    createVerticalTableRow("Cluster API Key", cluster.api_key),
-    createVerticalTableRow("Account", userEmail || "")
-  );
-
-  console.log(table.toString());
+  const configYAML = yaml.safeDump(argv.context.skycliConfig);
+  console.log(configYAML);
   return Promise.resolve();
 }
 
